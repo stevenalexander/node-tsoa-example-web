4f96093c65933c4062277fbaa952d52ffde4d6c0
Steven Alexander <s.alexander@kainos.com>
2018-04-13 14:13:52 +0100

    updated test to use injected mock

--- tests/unit/controllers/IndexControllerTests.ts
+++ tests/unit/controllers/IndexControllerTests.ts
@@ -1,30 +1,27 @@
 import 'mocha'
+import { expect } from 'chai'
 import * as supertest from 'supertest'
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
+import { mock, instance, when, verify } from 'ts-mockito'
 import { TYPES } from '../../../src/types'
 import { iocContainer } from '../../../src/ioc'
-import { User, UserCreationRequest } from 'tsoa-example-models'
+import { User } from 'tsoa-example-models'
 import { MockViewEngine } from '../mockViewEngine'
-import { UserClientInterface } from '../../../src/services/userClient'
+import { UserClient } from '../../../src/services/userClient'
 import { IndexController } from '../../../src/controllers/indexController'
-
-class MockUserClient implements UserClientInterface {
-  user: User = {
-    id: 1234,
-    name: 'Name',
-    email: 'email',
-    phoneNumbers: []
-  }
-
-  getAll(): Promise<User[]> { return Promise.resolve<User[]>([this.user]) }
-  get(id: number): Promise<User> { return Promise.resolve<User>(this.user) }
-  create(userCreationRequest: UserCreationRequest): Promise<void> { return Promise.resolve() }
-}
 
 describe('IndexController', function () {
   let request
   let indexController
+  let mockUserClient: UserClient
+  let user1: User = {
+    id: 1234,
+    email: 'string',
+    name: 'Name',
+    phoneNumbers: [],
+    status: 'status'
+  }
 
   beforeEach(function () {
     const app = express()
@@ -35,12 +32,8 @@ describe('IndexController', function () {
     const router = express.Router()
     app.use('/', router)
 
-    const mockUserClient = new MockUserClient
-    router.use(function (req, res, next) {
-      req['userClient'] = mockUserClient
-      next()
-    })
-    iocContainer.rebind(TYPES.UserClientInterface).toConstantValue(mockUserClient)
+    mockUserClient = mock(UserClient)
+    iocContainer.rebind(TYPES.UserClientInterface).toConstantValue(instance(mockUserClient))
 
     indexController = iocContainer.get<IndexController>(TYPES.IndexController)
     indexController.attachRoutes(router)
@@ -50,9 +43,15 @@ describe('IndexController', function () {
 
   describe('GET /', function () {
     it('should return index response', () => {
+      when(mockUserClient.getAll()).thenResolve([user1])
+
       return request
         .get('/')
         .expect(200)
+        .then(res => {
+          expect(res.text).to.contain(user1.name)
+          verify(mockUserClient.getAll()).once()
+        })
     })
   })
 })
